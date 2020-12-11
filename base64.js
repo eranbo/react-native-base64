e79f48f0efef4f5767185abeaed6a400cf3e77c5
eranbo <me@eranbo.com>
2020-12-11 23:23:51 +0200

    version 0.1.1 - throw error instead if window.alert

--- base64.js
+++ base64.js
@@ -43,9 +43,9 @@ export default {
         // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
         var base64test = /[^A-Za-z0-9\+\/\=]/g;
         if (base64test.exec(input)) {
-            window.alert("There were invalid base64 characters in the input text.\n" +
-                "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-                "Expect errors in decoding.");
+            throw new Error("There were invalid base64 characters in the input text.\n" +
+              "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+              "Expect errors in decoding.");
         }
         input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 
